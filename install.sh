c9a919df7a065b270833e0bd8bd5a03a5ebb3fc0
nlisgo <nathan@aeopus.com>
2017-01-13 17:30:23 +0000

    Add script to export and import migrate d2d configuration

--- install.sh
+++ install.sh
@@ -10,3 +10,8 @@ if [ -f sites/default/settings.php ] ; then
 fi
 
 /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush site-install spgb_profile --account-name=admin --account-pass=admin --site-name="SPGB" --yes
+
+if [ -f ../build/migrate_d2d_config.sql ] ; then
+    cd "$SCRIPTPATH/web"
+    drush sql-cli < ../build/migrate_d2d_config.sql
+fi
---
+++ migrate-d2d-config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e # all commands must pass
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+cd "$SCRIPTPATH/web"
+drush sqlq "SELECT name, title, HEX(arguments) FROM migrate_group WHERE title ='The Socialist Party of Great Britain'" | while IFS=$'\t' read name title arguments; do
+  echo "INSERT INTO \`migrate_group\` (\`name\`, \`title\`, \`arguments\`) VALUES ('$name','$title',X'$arguments');" > ../build/migrate_d2d_config.sql
+  query="SELECT machine_name, class_name, group_name, status, HEX(arguments) FROM migrate_status WHERE machine_name LIKE '"
+  query+=$name
+  query+="%';"
+  drush sqlq "$query" | while IFS=$'\t' read machine_name class_name group_name status arguments; do
+    echo "INSERT INTO \`migrate_status\` (\`machine_name\`, \`class_name\`, \`group_name\`, \`status\`, \`arguments\`) VALUES ('$machine_name','$class_name','$group_name',$status,X'$arguments');" >> ../build/migrate_d2d_config.sql
+  done
+  query="drush sql-dump --tables-list=migrate_field_mapping,migrate_map_"
+  query+=$name
+  query+="*,migrate_message_"
+  query+=$name
+  query+="*"
+  $query >> ../build/migrate_d2d_config.sql
+done
