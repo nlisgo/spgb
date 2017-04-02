3dbe5d5b670c25efd6cff0ab89175dabbec491d8
nlisgo <nathan@aeopus.com>
2017-04-03 00:53:24 +0100

    Update core and contrib and preserve site files

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -e # all commands must pass
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
 cd "$SCRIPTPATH"
+mkdir -p ./build/files
 if [ -d ./web/ ]; then
   chmod -R 777 ./web/
   rm -rf ./web
@@ -11,5 +12,6 @@ fi
 drush make ./src/drupal.make.yml ./web/spgb
 ln -s "../../../src/spgb_profile" "$SCRIPTPATH/web/spgb/profiles/"
 ln -s "../../../../src/settings.php" "$SCRIPTPATH/web/spgb/sites/default/"
+ln -s "../../../../build/files" "$SCRIPTPATH/web/spgb/sites/default/"
 chmod 755 ./web/spgb/sites/default/settings.php
 ./remake.sh
