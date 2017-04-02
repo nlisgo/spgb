a5e29f9f00e881d8ac1e8d1b2a3360ae31afcaf4
nlisgo <nathan@aeopus.com>
2017-04-02 18:25:03 +0100

    Add digitalocean setup instructions and amend install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if [ -f sites/default/settings.php ] ; then
     chmod 755 sites/default/settings.php
 fi
 
-/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush site-install spgb_profile --account-name=admin --account-pass=admin --site-name="SPGB" --yes
+/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush site-install spgb_profile --site-name="SPGB" --yes
 
 cd "$SCRIPTPATH/web/spgb"
 if [ -f ../../build/migrate_d2d_config.sql ] ; then
